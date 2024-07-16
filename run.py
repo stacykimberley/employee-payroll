7d78cc9e6f464e9c0a8fd8cf7afd870757f7e78b
Stacy Kimberley Hawadi <stacyhawadi@icloud.com>
2024-07-16 00:03:04 +0000

    Add data request loop to request data until it's valid

--- run.py
+++ run.py
@@ -6,23 +6,32 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
+
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('employee_payroll')
 
+
 def collect_total_hours():
     """
     Ask the user to input total hours data for each employee
     """
-    print("Please Enter total hours from the previous week.")
-    print("Data should be five numbers from emp001 to emp005, separated by commas.")
-    print("Like this: 40,40,40,20,20\n")
+    while True:
+        print("Please Enter total hours from the previous week.")
+        print("Data should be five numbers from emp001 to emp005, separated by commas.")
+        print("Like this: 40,40,40,20,20\n")
 
-    data_str = input("Enter total hours here: ")
-    
-    total_hours_data = data_str.split(",")
-    validate_data(total_hours_data)
+        data_str = input("Enter total hours here: ")
+        
+        total_hours_data = data_str.split(",")
+
+        if validate_data(total_hours_data):
+            print("Data is valid!")
+            break
+
+    return total_hours_data   
+
 
 def validate_data(values):
     """
@@ -30,7 +39,6 @@ def validate_data(values):
     Raises ValueError if string cannot be converted into float,
     or if there aren't exactly five values.
     """
-    print(values)
     try:
         [float(value) for value in values]
         if len(values) != 5:
@@ -39,6 +47,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True 
 
  
-collect_total_hours()    
+data = collect_total_hours()
